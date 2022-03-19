e4b2a740f5defbf1a4ebdfa36887b2e25902d690
GeorgeDoc <64958579+GeorgeDoc@users.noreply.github.com>
2022-03-19 18:15:55 +0200

    merged all

--- ProjectBlue_01/Classes/Product.cs
+++ ProjectBlue_01/Classes/Product.cs
@@ -8,9 +8,6 @@ namespace Classes
 {
     public class Product : Base
     {
-        //public Guid Id { get; set; }
-        //public string Code { get; set; }
-        //public string Description { get; set; }
         public Guid ProductID { get; set; }
         public double Price { get; set; }
         //this I dont know the use of
